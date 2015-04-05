3178a38effc8044960f3bafe6df6756c4b620ae3
kingpabel <imtiazpabel@yahoo.com>
2015-04-06 01:49:43 +0600

    mail css in inline

--- resources/views/eventMail.blade.php
+++ resources/views/eventMail.blade.php
@@ -3,7 +3,6 @@
 <head>
     <title>Kingpabel Scheduler List</title>
 
-    {!! HTML::style('css/bootstrap.min.css') !!}
 <style>/*!
  * Bootstrap v3.3.2 (http://getbootstrap.com)
  * Copyright 2011-2015 Twitter, Inc.
